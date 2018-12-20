6f166b41ae5109a9702b17a2ffa23ff5111098f6
denditopratama <denditoprtm@gmail.com>
2018-12-20 12:03:25 +0700

    perbaikan status aktif

--- rekapjmrb.php
+++ rekapjmrb.php
@@ -21,11 +21,12 @@ echo'
 </tr>';
 $no=1;
 $notals=1;
-$mon=mysqli_query($config,"SELECT * FROM tbl_user WHERE jmrb=0 AND(admin<>1 AND admin<>9 AND id_user<>9999) ORDER BY status_aktif,admin");
+$mon=mysqli_query($config,"SELECT * FROM tbl_user WHERE jmrb=0 AND(admin<>1 AND admin<>9 AND id_user<>9999 AND status_aktif=1) ORDER BY status_aktif,admin");
 while($row=mysqli_fetch_array($mon)){
 	echo'
 	<tr>
 	<td>'.$no++.'</td>
+	<td>'.$row['nip'].'</td>
 	<td>'.$row['nama'].'</td>';
 	$mb=mysqli_query($config,"SELECT jabatan FROM tbl_ref_jabatan WHERE id='".$row['jabatan']."'");
 	list($jabatan)=mysqli_fetch_array($mb);
@@ -60,11 +61,12 @@ echo'
 </tr>';
 $no=1;
 $notal=1;
-$mon=mysqli_query($config,"SELECT * FROM tbl_user WHERE jmrb=1 AND(admin<>1 AND admin<>9 AND id_user<>9999) ORDER BY status_aktif,admin");
+$mon=mysqli_query($config,"SELECT * FROM tbl_user WHERE jmrb=1 AND(admin<>1 AND admin<>9 AND id_user<>9999 AND status_aktif=1) ORDER BY status_aktif,admin");
 while($row=mysqli_fetch_array($mon)){
 	echo'
 	<tr>
 	<td>'.$no++.'</td>
+	<td>'.$row['nip'].'</td>
 	<td>'.$row['nama'].'</td>';
 	$mb=mysqli_query($config,"SELECT jabatan FROM tbl_ref_jabatan WHERE id='".$row['jabatan']."'");
 	list($jabatan)=mysqli_fetch_array($mb);
