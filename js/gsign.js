b126bfb9b4ea7d12c875615eb96d3a9b532f6e0a
denditopratama <denditoprtm@gmail.com>
2019-01-11 02:21:35 +0700

    Asd

--- js/gsign.js
+++ js/gsign.js
@@ -13,6 +13,21 @@
     
                      if(data==' xx'){
                       location.reload();
+                        gapi.load('auth2', function() {
+                            gapi.auth2.init({
+                  client_id: '49446115720-gacrc8lhqmdj9rpn3efdpdsa3kh74usu.apps.googleusercontent.com'
+                }).then(function(){
+                                var auth2 = gapi.auth2.getAuthInstance();
+                                auth2.signOut().then(function(){
+                                  auth2.disconnect();
+                                    window.location = "./";
+                                    
+                                });
+                                
+                            });
+                
+                        });
+                    
                      } else {
                        window.location.href=data;
                      }
