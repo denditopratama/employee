3123d774718b1f6c0e24e2a0cf59d8f97251435c
denditopratama <denditoprtm@gmail.com>
2019-01-11 01:58:47 +0700

    perbaik bug

--- js/gsign.js
+++ js/gsign.js
@@ -4,18 +4,22 @@
         // Useful data for your client-side scripts:
         var profile = googleUser.getBasicProfile();
             
+        var sig = googleUser.isSignedIn.get();
 
         // The ID token you need to pass to your backend:
         var id_token = googleUser.getAuthResponse().id_token;
        
-        if(profile){
-         $.post('js/login_gmail.php',{id_token:id_token}, function(data){
-                     
-                           window.location.href=data;
+        if(sig==false){
+          if(profile){
+            $.post('js/login_gmail.php',{id_token:id_token}, function(data){
+                        
+                              window.location.href=data;
+                       
                     
-                 
-              });
+                 });
+           }
         }
+        
 
       }
       
