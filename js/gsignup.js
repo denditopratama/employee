629b496f8997d8f90b149bff98d7868710ff49e6
denditopratama <denditoprtm@gmail.com>
2019-01-11 18:26:58 +0700

    tes

--- js/gsignup.js
+++ js/gsignup.js
@@ -7,29 +7,31 @@
 
         // The ID token you need to pass to your backend:
         var id_token = googleUser.getAuthResponse().id_token;
-        
-        if(profile){
-          var kong = jQuery('#namyeng').val();
-          
-         $.post('js/login_gmail.php',{id_token:id_token, daftar : 1, kong : kong}, function(data){
-                    
-                    
-          if(data==' y'){
+  
+          if(profile){
+            var kong = jQuery('#namyeng').val();
             
-            var auth2 = gapi.auth2.getAuthInstance();
-            auth2.signOut().then(function () {
-              console.log(kong);
-              window.location.href='./admin.php?page=usr&gmail=1';
-            });
-          
-           } else {
-             window.location.href=data;
-           }
-                 
-                    
-                 
+           $.post('js/login_gmail.php',{id_token:id_token, daftar : 1, kong : kong}, function(data){
+                      
+                      
+            if(data==' y'){
+              
+              var auth2 = gapi.auth2.getAuthInstance();
+              auth2.signOut().then(function () {
+                console.log(kong);
+                window.location.href='./admin.php?page=usr&gmail=1';
               });
-        }
+            
+             } else {
+               window.location.href=data;
+             }
+                   
+                      
+                   
+                });
+          }
+       
+        
 
       }
       
