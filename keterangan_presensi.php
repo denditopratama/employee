7068fc171e846d1ca10339d213716ef6207d0d0c
denditopratama <denditoprtm@gmail.com>
2019-02-18 18:08:01 +0700

    perbaikan sisi manager lembur

--- keterangan_presensi.php
+++ keterangan_presensi.php
@@ -233,7 +233,7 @@
                 tbl_status_keterangan_presensi.id_user=tbl_user.id_user AND tbl_user.divisi='".$_SESSION['divisi']."' ORDER by tbl_user.admin");  
             } else if ($_SESSION['admin']==5) {
                 $gk=mysqli_query($config,"SELECT tbl_status_keterangan_presensi.*,tbl_user.divisi FROM tbl_status_keterangan_presensi,tbl_user WHERE id_presensi='$id' AND 
-                tbl_status_keterangan_presensi.id_user=tbl_user.id_user AND(tbl_user.divisi='".$_SESSION['divisi']."' AND tbl_user.admin<>4) ORDER by tbl_user.admin");  
+                tbl_status_keterangan_presensi.id_user=tbl_user.id_user AND(tbl_user.divisi='".$_SESSION['divisi']."' AND tbl_user.admin>5) ORDER by tbl_user.admin");  
             } else {
                 $gk=mysqli_query($config,"SELECT DISTINCT tbl_status_keterangan_presensi.* FROM tbl_status_keterangan_presensi,tbl_user WHERE id_presensi='$id' AND 
                 tbl_status_keterangan_presensi.id_user='".$_SESSION['id_user']."' ORDER by tbl_user.admin");  
