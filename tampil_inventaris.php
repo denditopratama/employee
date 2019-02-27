ea681d4dd6e3d3380333f0a7d1114c9624246652
denditopratama <denditoprtm@gmail.com>
2019-02-27 22:53:22 +0700

    test

--- tampil_inventaris.php
+++ tampil_inventaris.php
@@ -1,8 +1,8 @@
 <?php
 include('/include/config.php');
-$idd=mysqli_real_escape_string($config,$_GET['id']);
-$id=base64_decode($idd);
-$gow=mysqli_query($config,"SELECT * FROM tbl_inventaris WHERE id_invent='$id'");
+$idd=mysqli_real_escape_string($config,base64_decode($_GET['id']));
+
+$gow=mysqli_query($config,"SELECT * FROM tbl_inventaris WHERE id_invent='$idd'");
 while($row=mysqli_fetch_array($gow)){
     echo '
     <h5>Nama Barang : '.$row['nama_barang'].'
